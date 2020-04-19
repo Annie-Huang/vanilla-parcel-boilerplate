3147cb9e84eb3db351bc56811fe41028c08aa7fc
Annie Huang <annie.hn@gmail.com>
2020-04-19 15:18:37 +1000

    Add User.js component into app.js, which will make a axio call to https://randomuser.me/api to fetch data.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import Header from "./components/Header";
+import User from "./components/User";
+
 import './scss/app.scss';
 
 // console.log('It works!')
 
-const app = () => {
+const app = async () => {
     document.getElementById('header').innerHTML = Header();
+    document.getElementById('user').innerHTML = await User();
 }
 
 // Init app
