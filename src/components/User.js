4fb44523d5cf6ac8446f99f89185c7658a17d2ef
Annie Huang <annie.hn@gmail.com>
2020-04-19 15:22:28 +1000

    Display user info in the User.js

--- src/components/User.js
+++ src/components/User.js
@@ -4,9 +4,21 @@ const User = async () => {
     const res = await axios.get('https://randomuser.me/api');
     const user = res.data.results[0];
 
-    console.log(user);
+    // console.log(user);
 
-    const template = `User`;
+    const template = `
+        <div class="card">
+            <img src="${user.picture.large}" alt="">
+            <div class="card-body">
+                <h1>${user.name.first} ${user.name.last}</h1>
+                <ul>
+                    <li>${user.email}</li>
+                    <li>${user.phone}</li>
+                    <li>${user.location.city}</li>
+                </ul>
+            </div>
+        </div>
+    `;
 
     return template;
 }
