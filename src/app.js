92081cc86643e8e1ccbed7707135099939b9d18c
Annie Huang <annie.hn@gmail.com>
2020-04-19 15:02:08 +1000

    Inject a Header.js component into the id='header' location in index.html

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+import Header from "./components/Header";
 import './scss/app.scss';
 
-console.log('It works!')
+// console.log('It works!')
+
+const app = () => {
+    document.getElementById('header').innerHTML = Header();
+}
+
+// Init app
+app();
